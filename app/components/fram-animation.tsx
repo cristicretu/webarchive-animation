aed84f9e026598c77fbd1f83a835fcad0eeda93e
Cristian Cretu <crisemcr@gmail.com>
2024-05-09 19:25:41 +0300

    init

---
+++ app/components/fram-animation.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { AnimatePresence, MotionConfig, motion, spring } from "framer-motion";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+
+const IMAGES = ["japan", "jungle", "new-york", "desert"];
+
+export function FrameAnimation() {
+  return <></>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { FrameAnimation } from "./components/fram-animation";
+
+export default function Home() {
+  return (
+    <div className="h-screen items-center justify-center flex">
+      <FrameAnimation />
+    </div>
+  );
+}
